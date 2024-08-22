f80316f9680730ea1ba622f37c8897f888c6903f
kmho1799 <kmho1799@naver.com>
2024-08-23 01:09:28 +0900

    add index.tsx (redux)

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
+
+// 스토어 생성
+const store = createStore(counter);
+
+// 액션을 추가해서 스토어에 전달
+// 사진에서 UI안의 Deposit, Withdraw처럼
+// root.render(
+//   <React.StrictMode>
+//     <App 
+//       value={store.getState()}
+//       onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+//       onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+//     />
+//   </React.StrictMode>
+// );
+
+const render = () => root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />
   </React.StrictMode>
 );
 
+render();
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
